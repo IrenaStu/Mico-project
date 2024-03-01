ec5ac11cd209be28afedbd07245c7b00f7b51c06
900KILO <antoni.lomsadze@gmail.com>
2024-03-01 23:46:58 +0400

    make appointment full function

--- index.php
+++ index.php
@@ -75,71 +75,11 @@
 
   <!-- book section -->
   
+  <?php
+  mainAppointment();
+  ?>
 
-  <section class="book_section layout_padding">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <form action = "page.php" method = "POST" >
-          
   
-
-    
-
-    
-
-            <h4>
-              BOOK <span>APPOINTMENT</span>
-            </h4>
-            <div class="form-row ">
-              <div class="form-group col-lg-4">
-              <label for="inputPatientName">Patient Name</label>
-              <input type="text" class="form-control" name="inputPatientName" id="inputPatientName" placeholder="Patient Name" required>
-              </div>
-              <div class="form-group col-lg-4">
-              <label for="inputDoctorName">Doctor's Name</label>
-                <select name="inputDoctorName" class="form-control wide" id="inputDoctorName" required>
-                    <option value="Dr. Morco">Dr. Morco</option>
-                    <option value="Dr. Hennry">Dr. Hennry</option>
-                    <option value="Dr. Jenni">Dr. Jenni</option>
-                </select>
-              </div>
-              <div class="form-group col-lg-4">
-                <label for="inputDepartmentName">Department's Name</label>
-                <select name="inputDepartmentName" class="form-control wide" id="inputDepartmentName" required>
-                  <option value="First">First</option>
-                  <option value="Second">Second</option>
-                  <option value="Third">Third</option>
-                </select>
-              </div>
-            </div>
-            <div class="form-row ">
-              <div class="form-group col-lg-4">
-                <label for="inputPhone">Phone Number</label>
-                <input type="number" class="form-control" name="inputPhone" id="inputPhone" placeholder="XXXXXXXXXX" required>
-              </div>
-              <div class="form-group col-lg-4">
-                <label for="inputSymptoms">Symptoms</label>
-                <input type="text" class="form-control" name="inputSymptoms" id="inputSymptoms" placeholder="Symptoms" required>
-              </div>
-              <div class="form-group col-lg-4">
-                <label for="inputDate">Choose Date </label>
-                <div class="input-group date" name="inputDate" id="inputDate" data-date-format="mm-dd-yyyy" required>
-                  <input type="text" class="form-control" name="inputDate" id="inputDate"  readonly required>
-                  <span class="input-group-addon date_icon">
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div class="btn-box">
-              <button type="submit" class="btn ">Submit Now</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
 
 
   <!-- end book section -->
